5fa03424f0802d60f62d8c9ff57632fac927a65a
chinmay-bhat-impelsys <chinmay.bhat@impelsys.com>
2021-05-19 19:34:07 +0530

    AS-15 refactored choke control

--- src/containers/controls/choke.control.js
+++ src/containers/controls/choke.control.js
@@ -2,23 +2,36 @@ const Alexa = require('ask-sdk-core');
 
 const { InputUtil, Control, RequestValueAct } = require('ask-sdk-controls');
 
-const {
-    prepareScreenContent,
-    imageCatalog,
-    displayTemplate,
-    displayDirective,
-    repeatText,
-    speakText,
-} = require('../../common/util');
+const { speakText, renderGeneralFunction, assets, configData } = require('../../common/util');
 
 const chokeData = require('../../common/content/choke.content.json');
-const dataTemplate = require('../../common/content/dataTemplate.json');
 
 const { chokeMainText } = speakText;
 
 const ChokeControlState = {
     value: undefined,
 };
+
+class ChokeActMain extends RequestValueAct {
+    constructor(control, payload) {
+        super(control, payload);
+        this.speechText = undefined;
+        this.imageUrl = undefined;
+        this.primaryText = undefined;
+        this.secondaryText = undefined;
+    }
+
+    render(input, resultBuilder) {
+        resultBuilder = renderGeneralFunction(
+            input,
+            resultBuilder,
+            this.speechText,
+            this.imageUrl,
+            this.primaryText,
+            this.secondaryText
+        );
+    }
+}
 
 class ChokeControl extends Control {
     constructor(props) {
@@ -30,108 +43,58 @@ class ChokeControl extends Control {
         if (InputUtil.isIntent(input, 'chokeIntent')) {
             return true;
         } else if (InputUtil.isIntent(input, 'AMAZON.YesIntent')) {
-            /* if (this.state && this.state.includes('choking')) {
-                return true;
-            } */
             return true;
         } else if (InputUtil.isIntent(input, 'AMAZON.NoIntent')) {
-            /* if (this.state && this.state.includes('choking')) {
-                return true;
-            } */
             return true;
         }
         return false;
     }
 
     handle(input, resultBuilder) {
-        resultBuilder.addAct(new RequestValueAct(this, {}));
+        const chokeValueAct = new ChokeActMain(this, {});
+        if (InputUtil.isIntent(input, 'chokeIntent')) {
+            this.state = 'chokingInfo';
+
+            chokeValueAct.speechText = chokeMainText;
+            chokeValueAct.imageUrl = `https://${configData[process.env.ENVIRONMENT].cloudfront}/${
+                assets.Images['choke.control.main']
+            }`;
+            chokeValueAct.primaryText = 'Is the person pregnant? ';
+            chokeValueAct.secondaryText = '';
+        } else if (InputUtil.isIntent(input, 'AMAZON.YesIntent')) {
+            const topic = this.state;
+            const topicnext = chokeData[topic].yes.continue;
+            if (topicnext === false) {
+                this.state = undefined;
+            } else {
+                this.state = topicnext;
+            }
+            chokeValueAct.speechText = chokeData[topic].yes.speechText;
+            chokeValueAct.imageUrl = `https://${configData[process.env.ENVIRONMENT].cloudfront}/${
+                chokeData[topic].yes.imageUrl
+            }`;
+            chokeValueAct.primaryText = chokeData[topic].yes.primaryText;
+            chokeValueAct.secondaryText = chokeData[topic].yes.secondaryText;
+        } else if (InputUtil.isIntent(input, 'AMAZON.NoIntent')) {
+            const topic = this.state;
+            const topicnext = chokeData[topic].no.continue;
+            if (topicnext === false) {
+                this.state = undefined;
+            } else {
+                this.state = topicnext;
+            }
+            chokeValueAct.speechText = chokeData[topic].no.speechText;
+            chokeValueAct.imageUrl = `https://${configData[process.env.ENVIRONMENT].cloudfront}/${
+                chokeData[topic].no.imageUrl
+            }`;
+            chokeValueAct.primaryText = chokeData[topic].no.primaryText;
+            chokeValueAct.secondaryText = chokeData[topic].no.secondaryText;
+        }
+        resultBuilder.addAct(chokeValueAct);
     }
 
     canTakeInitiative() {
         return false;
-    }
-
-    renderAct(act, input, responseBuilder) {
-        if (act instanceof RequestValueAct) {
-            if (InputUtil.isIntent(input, 'chokeIntent')) {
-                this.state = 'chokingInfo';
-                const resp = chokeMainText;
-                responseBuilder.addPromptFragment(resp);
-                responseBuilder.addRepromptFragment(repeatText);
-                if (
-                    Alexa.getSupportedInterfaces(input.handlerInput.requestEnvelope)[
-                        'Alexa.Presentation.APL'
-                    ]
-                ) {
-                    const chokeImage =
-                        'https://s3.amazonaws.com/ahaalexa/forechoshow/Chest_Thrust_AdultFloor.jpg';
-                    dataTemplate.content.primaryText = 'Choking person';
-                    dataTemplate.content.bodyText = resp;
-                    dataTemplate.content.mainImage = chokeImage;
-                    responseBuilder.addDirective({
-                        type: 'Alexa.Presentation.APL.RenderDocument',
-                        document: displayTemplate,
-                        datasources: dataTemplate,
-                    });
-                }
-            } else if (InputUtil.isIntent(input, 'AMAZON.YesIntent')) {
-                const topic = this.state;
-                const { speechText } = chokeData[topic].yes;
-                const { primaryText } = chokeData[topic].yes;
-                const { secondaryText } = chokeData[topic].yes;
-                const { imageUrl } = chokeData[topic].yes;
-                const topicnext = chokeData[topic].yes.continue;
-                if (topicnext === false) {
-                    this.state = 'unknown';
-                } else {
-                    this.state = topicnext;
-                }
-                responseBuilder.addPromptFragment(speechText);
-                responseBuilder.addRepromptFragment(repeatText);
-                if (
-                    Alexa.getSupportedInterfaces(input.handlerInput.requestEnvelope)[
-                        'Alexa.Presentation.APL'
-                    ]
-                ) {
-                    dataTemplate.content.primaryText = primaryText;
-                    dataTemplate.content.bodyText = secondaryText;
-                    dataTemplate.content.mainImage = imageUrl;
-                    responseBuilder.addDirective({
-                        type: 'Alexa.Presentation.APL.RenderDocument',
-                        document: displayTemplate,
-                        datasources: dataTemplate,
-                    });
-                }
-            } else if (InputUtil.isIntent(input, 'AMAZON.NoIntent')) {
-                const topic = this.state;
-                const { speechText } = chokeData[topic].no;
-                const { primaryText } = chokeData[topic].no;
-                const { secondaryText } = chokeData[topic].no;
-                const { imageUrl } = chokeData[topic].no;
-                const topicnext = chokeData[topic].no.continue;
-                if (topicnext === false) {
-                    this.state = 'unknown';
-                } else {
-                    this.state = topicnext;
-                }
-                responseBuilder.addPromptFragment(speechText);
-                responseBuilder.addRepromptFragment(repeatText);
-                if (
-                    Alexa.getSupportedInterfaces(input.handlerInput.requestEnvelope)[
-                        'Alexa.Presentation.APL'
-                    ]
-                ) {
-                    dataTemplate.content.primaryText = primaryText;
-                    dataTemplate.content.bodyText = secondaryText;
-                    dataTemplate.content.mainImage = imageUrl;
-                    responseBuilder.addDirective({
-                        type: 'Alexa.Presentation.APL.RenderDocument',
-                        document: displayTemplate,
-                        datasources: dataTemplate,
-                    });
-                }
-            }
-        }
     }
 }
 module.exports = ChokeControl;
