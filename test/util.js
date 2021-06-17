8f229a80f5f98861a2d84653bdb6c30126bb1b05
Sandeep Yadav <t-sandeep.yadav@heart.org>
2021-06-17 08:54:55 +0530

    Fix: AS-4 Choke intent test case possible combination logic added

--- test/util.js
+++ test/util.js
@@ -26,7 +26,35 @@ async function testIntentRequest(intentName, inputText, speakText) {
     expect(intentResponse.response.shouldEndSession).equals(false);
 }
 
+const getAllPossibleTestCases = (key, input) => {
+    const output2DArray = [];
+    const iterateObject = (objectKey, path) => {
+        if (objectKey.yes) {
+            path.push('yes');
+            if (!objectKey.yes.continue) {
+                output2DArray.push(Object.assign([], path));
+                path.pop();
+            } else {
+                iterateObject(input[objectKey.yes.continue], path);
+            }
+        }
+        if (objectKey.no) {
+            path.push('no');
+            if (!objectKey.no.continue) {
+                output2DArray.push(Object.assign([], path));
+                path.pop();
+            } else {
+                iterateObject(input[objectKey.no.continue], path);
+            }
+        }
+        path.pop();
+    };
+    iterateObject(key, []);
+    return output2DArray;
+};
+
 module.exports = {
     testLaunchRequest,
     testIntentRequest,
+    getAllPossibleTestCases,
 };
