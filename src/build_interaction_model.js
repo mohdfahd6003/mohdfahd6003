1bb40d2dad2adba2c773c4d83acb0c33a2a04341
Sandeep Yadav <t-sandeep.yadav@heart.org>
2021-06-18 08:42:33 +0530

    fix: AS-4 interaction model improvement

--- src/build_interaction_model.js
+++ src/build_interaction_model.js
@@ -10,19 +10,48 @@ new ControlInteractionModelGenerator()
     .addIntent({ name: 'AMAZON.NavigateHomeIntent' })
     .addIntent({ name: 'AMAZON.HelpIntent' })
     .addIntent({ name: 'AMAZON.CancelIntent' })
-    .addIntent({ name: 'HelloIntent', samples: ['Say hello', 'Say hi'] })
+    .addIntent({ name: 'HelloIntent', samples: ['hello', 'hi', 'hey', 'hey there', 'hi there'] })
     .addIntent({
         name: 'heartWarningSignsIntent',
-        samples: ['Warning signs of a heart attack', 'How do I know about heart attack'],
+        samples: [
+            'Warning signs of a heart attack',
+            'How do I know about heart attack',
+            'signs of heart attack',
+            'what are the signs of heart attack?',
+            'tell me the signs of heart attack',
+            'what could be the signs of heart attack?',
+            'possible signs of heart attack',
+            'help me heart attack warning signs',
+            'symptoms of heart attack',
+            'heart attack warning signs',
+        ],
     })
     .addIntent({
         name: 'learnCPRIntent',
-        samples: ['how to do c. p. r.', 'how do I do c.p.r', 'How do I do CPR?', 'learn cpr'],
+        samples: [
+            'how to do c. p. r.',
+            'how do I do c.p.r',
+            'How do I do CPR?',
+            'learn cpr',
+            'how can I perform CPR',
+            'what are the steps to do CPR?',
+            'how to help some one with CPR?',
+            'give me the steps to do CPR',
+            'help me to do CPR',
+        ],
     })
     .addYesAndNoIntents()
     .addIntent({
         name: 'bleedIntent',
-        samples: ['I cut myself.  What do I do', 'I cut myself. What do I do?'],
+        samples: [
+            'I cut myself.  What do I do',
+            'I cut myself. What do I do?',
+            'I have been cut. What shall I do?',
+            'I have cut myself with sharp object. What do I do?',
+            'Can you provide me help, I have cut myself?',
+            'tell me how to give fist aid for cut',
+            'learn about cut fist aid',
+        ],
     })
     .addIntent({
         name: 'chokeIntent',
@@ -31,27 +60,75 @@ new ControlInteractionModelGenerator()
             'need assistance with choking',
             'warning signs of choking',
             'Tell me what to do for choking',
+            'What should be done in case of choking',
+            'help me with choking',
+            'I need assistance for choking',
         ],
     })
     .addIntent({
         name: 'burnIntent',
-        samples: ['burning', 'What do I do for a burn?'],
+        samples: [
+            'burning',
+            'What do I do for a burn?',
+            'need assistance in burning',
+            'how to help some with burn',
+            'help me with burn',
+            'What shall I don in case of burn?',
+            'what to do for burn?',
+        ],
     })
     .addIntent({
         name: 'dehydrationIntent',
-        samples: ['dehydration', 'What is Dehydration?', 'explain me on Dehydrationec'],
+        samples: [
+            'dehydration',
+            'What is Dehydration?',
+            'explain me on Dehydration',
+            'I am feeling weakness and dryness in mouth',
+            'help me with dehydration',
+            'what to do in case of dehydration?',
+            'how to get rid of dehydration?',
+            'what are the symptoms of dehydration?',
+        ],
     })
     .addIntent({
         name: 'noseIntent',
-        samples: ['nose bleeding', 'How do I stop a Nose Bleed?'],
+        samples: [
+            'nose bleeding',
+            'How do I stop a Nose Bleed?',
+            'my nose is bleeding. What shall I do?',
+            'I have nose bleeding',
+            'how can I stop nose bleeding?',
+            'get me the steps to stop nose bleeding',
+            'tell me how to stop nose bleeding',
+            'my nose is bleeding. How to stop bleeding?',
+        ],
     })
     .addIntent({
         name: 'poisonIntent',
-        samples: ['poisoning', 'Tell me what to do for poisoning'],
+        samples: [
+            'poisoning',
+            'Tell me what to do for poisoning',
+            'what to do in case of poisoning',
+            'how to help in poisoning',
+            'what shall I do in case of poisoning?',
+            'tell me about poisoning',
+            'what is poisoning?',
+        ],
     })
     .addIntent({
         name: 'strokeIntent',
-        samples: ['warning signs of stroke'],
+        samples: [
+            'warning signs of stroke',
+            'How do I know about stroke',
+            'signs of stroke',
+            'what are the signs of stroke?',
+            'tell me the signs of stroke',
+            'what could be the signs of stroke?',
+            'possible signs of stroke',
+            'help me stroke warning signs',
+            'symptoms of stroke',
+            'stroke warning signs',
+        ],
     })
     .buildCoreModelForControls(new RootManager())
 
