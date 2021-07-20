d778cc4d790d0d6cbcd9cb723d14c30b7d5adf19
chinmay-bhat-impelsys <chinmay.bhat@impelsys.com>
2021-07-20 16:36:55 +0530

    AS-42: Added box containers

--- src/common/utils/layoutBuilders/welcome/build.welcome.js
+++ src/common/utils/layoutBuilders/welcome/build.welcome.js
@@ -1,12 +1,34 @@
 const { welcome } = require('./getWelcomeLayouts');
 
+function boxContainerCreator() {
+    let boxContainer = {};
+    boxContainer = welcome.getWelcomeBoxContainer();
+    return boxContainer;
+}
+
+function boxWrapper() {
+    let boxWrap = {};
+    boxWrap = welcome.getWelcomeBoxWrapFrame();
+    boxWrap.items = [];
+    boxWrap.items.push(boxContainerCreator());
+    return boxWrap;
+}
+
+function boxCreator() {
+    let box = {};
+    box = welcome.getWelcomeBox();
+    box.items = [];
+    box.items.push(boxWrapper());
+    return box;
+}
+
 function welcomePageCreater() {
     let welcomeRootContainer = {};
     welcomeRootContainer = welcome.getWelcomeRoot();
     welcomeRootContainer.items = [];
     welcomeRootContainer.items.push(welcome.getWelcomeBackground());
-    welcomeRootContainer.items.push(welcome.getWelcomeImageBox());
-    welcomeRootContainer.items.push(welcome.getWelcomeTextBox());
+    welcomeRootContainer.items.push(welcome.getWelcomeImageBody());
+    welcomeRootContainer.items.push(boxCreator());
     return welcomeRootContainer;
 }
 
--- src/common/utils/layoutBuilders/welcome/getWelcomeLayouts.js
+++ src/common/utils/layoutBuilders/welcome/getWelcomeLayouts.js
@@ -1,7 +1,9 @@
 const rectWelcomeBackground = require('../../../display/layouts/welcome/background.welcome.json');
 const rectWelcomeRoot = require('../../../display/layouts/welcome/rootLayout.welcome.json');
-const rectWelcomeTextBox = require('../../../display/layouts/welcome/textBox.welcome.json');
-const rectWelcomeImageBox = require('../../../display/layouts/welcome/imageBox.welcome.json');
+const rectWelcomeBox = require('../../../display/layouts/welcome/box.welcome.json');
+const rectWelcomeImageBody = require('../../../display/layouts/welcome/imageBody.welcome.json');
+const rectWelcomeBoxWrapFrame = require('../../../display/layouts/welcome/boxWrapFrame.welcome.json');
+const rectWelcomeBoxContainer = require('../../../display/layouts/welcome/boxContainer.json');
 
 function getWelcomeBackground() {
     return rectWelcomeBackground;
@@ -11,17 +13,27 @@ function getWelcomeRoot() {
     return rectWelcomeRoot;
 }
 
-function getWelcomeTextBox() {
-    return rectWelcomeTextBox;
+function getWelcomeBox() {
+    return rectWelcomeBox;
 }
 
-function getWelcomeImageBox() {
-    return rectWelcomeImageBox;
+function getWelcomeImageBody() {
+    return rectWelcomeImageBody;
+}
+
+function getWelcomeBoxWrapFrame() {
+    return rectWelcomeBoxWrapFrame;
+}
+
+function getWelcomeBoxContainer() {
+    return rectWelcomeBoxContainer;
 }
 
 exports.welcome = {
     getWelcomeBackground,
     getWelcomeRoot,
-    getWelcomeTextBox,
-    getWelcomeImageBox,
+    getWelcomeBox,
+    getWelcomeImageBody,
+    getWelcomeBoxWrapFrame,
+    getWelcomeBoxContainer,
 };
