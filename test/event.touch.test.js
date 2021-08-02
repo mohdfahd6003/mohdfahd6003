6c6e18b90208aa8519d3fc27a5326fc91e41fa7a
chinmay-bhat-impelsys <chinmay.bhat@impelsys.com>
2021-08-02 17:59:45 +0530

    Added touch events

---
+++ test/event.touch.test.js
@@ -0,0 +1,44 @@
+const { ControlHandler, TestInput, SkillTester } = require('ask-sdk-controls');
+
+const { expect } = require('chai');
+
+const { waitForDebugger } = require('ask-sdk-controls');
+
+const { describe, test } = require('mocha');
+
+const { RootManager } = require('../src/index.js');
+
+const speakText = require('../src/common/content/constants.json');
+
+const { introText, heartAttackText } = speakText;
+
+waitForDebugger();
+
+describe('event touch path', () => {
+    test('Touch unit test :  heart attack', async () => {
+        await testIntentRequest('heartControl', heartAttackText);
+    });
+});
+
+function createTouchEvent(controlId) {
+    return TestInput.userEvent({
+        type: 'Alexa.Presentation.APL.UserEvent',
+        requestId: '12345',
+        timestamp: '2019-10-04T18:48:22Z',
+        locale: 'en-US',
+        arguments: ['one', 'two'],
+        components: {},
+        source: {
+            type: 'TouchWrapper',
+            handler: 'Press',
+            id: controlId,
+        },
+        token: '',
+    });
+}
+
+async function testIntentRequest(controlId, responseText) {
+    const tester = new SkillTester(new ControlHandler(new RootManager()));
+
+    await tester.testTurn('', createTouchEvent(controlId), `A:${responseText}`);
+}
