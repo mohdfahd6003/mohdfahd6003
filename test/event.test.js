cf41bf25de0a04512cbcaafc3751f668f2c38428
chinmay-bhat-impelsys <chinmay.bhat@impelsys.com>
2021-08-09 16:10:32 +0530

    AS:58 touch event test cases

--- test/event.test.js
+++ test/event.test.js
@@ -12,11 +12,60 @@ const speakText = require('../src/common/content/constants.json');
 
 const heartData = require('../src/common/content/heart.content.json');
 
+const strokeData = require('../src/common/content/strokes.content.json');
+
+const poisonData = require('../src/common/content/poison.content.json');
+
+const learncprData = require('../src/common/content/learncpr.content.json');
+
+const dehydrationData = require('../src/common/content/dehydration.content.json');
+
+const nosebleedData = require('../src/common/content/nosebleed.content.json');
+
+const cutData = require('../src/common/content/cut.content.json');
+
+const burnData = require('../src/common/content/burn.content.json');
+
+const { configData, assets } = require('../src/common/utils/util');
+
 waitForDebugger();
 
 describe('event touch path', () => {
-    test('Touch unit test :  heart attack', async () => {
+    test('touch unit test: heart attack', async () => {
         await testIntentRequest('heartControl', heartData);
+    });
+    test('touch unit test: strokes ', async () => {
+        await testIntentRequest('strokeControl', strokeData);
+    });
+    test('touch unit test: poison ', async () => {
+        await testIntentRequest('poisonControl', poisonData);
+    });
+    test('touch unit test: dehydration ', async () => {
+        await testIntentRequest('dehydrationControl', dehydrationData);
+    });
+    test('touch unit test: nose bleed ', async () => {
+        await testIntentRequest('nosebleedingControl', nosebleedData);
+    });
+
+    test('touch unit test: learn CPR ', async () => {
+        const { speakTextOne, speakTextTwo } = learncprData;
+        const combinedData = {};
+        combinedData.speakText = `${speakTextOne} <audio src="https://${
+            configData[process.env.ENVIRONMENT].cloudfront
+        }/${assets.Audio.learncprControl}"/> ${speakTextTwo}`;
+        await testIntentRequest('learncprControl', combinedData);
+    });
+
+    test('touch unit test: choke ', async () => {
+        const chokeData = {};
+        chokeData.speakText = speakText.chokeMainText;
+        await testIntentRequest('chokeControlMain', chokeData);
+    });
+    test('touch unit test: bleed ', async () => {
+        await testIntentRequest('cutControl', cutData.main);
+    });
+    test('touch unit test: burn ', async () => {
+        await testIntentRequest('burnControl', burnData.main);
     });
 });
 
